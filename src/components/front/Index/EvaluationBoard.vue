8310930d68f5c9c10e3dcbacf2703eaee961baa2
kathyhu91 <huchiachun@gmail.com>
2024-03-18 12:05:00 +0800

    fix(EvaluationBoard): fix style

--- src/components/front/Index/EvaluationBoard.vue
+++ src/components/front/Index/EvaluationBoard.vue
@@ -34,8 +34,10 @@
     &__container {
       width: 100%;
       height: 100%;
-      max-width: 416px;
       min-height: 328px;
+      @include media-breakpoint-up(lg) {
+        max-width: 416px;
+      }
     }
   }
   &__avatar {
