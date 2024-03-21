b1cefda2168b13b1e2c58b13dfc46591a35f8f2c
kathyhu91 <huchiachun@gmail.com>
2024-03-21 22:23:26 +0800

    feat(DifClassTable): add DifClassTable component in TrailsIntroView

--- src/views/front/FrontIndexView.vue
+++ src/views/front/FrontIndexView.vue
@@ -2,18 +2,19 @@
 
 <template>
   <div class="d-grid gap-5 bg-light-gray">
-    <IndexBanner />
+    <p>FrontIndex</p>
+    <!-- <IndexBanner />
     <BulletinBoard />
     <FeatureList />
     <GradedList />
-    <EvaluationBoard />
+    <EvaluationBoard /> -->
   </div>
 </template>
 
 <script setup>
-import IndexBanner from '@/components/front/Index/IndexBanner.vue'
-import BulletinBoard from '@/components/front/Index/BulletinBoard.vue'
-import FeatureList from '@/components/front/Index/FeatureList.vue'
-import GradedList from '@/components/front/Index/GradedList.vue'
-import EvaluationBoard from '@/components/front/Index/EvaluationBoard.vue'
+// import IndexBanner from '@/components/front/Index/IndexBanner.vue'
+// import BulletinBoard from '@/components/front/Index/BulletinBoard.vue'
+// import FeatureList from '@/components/front/Index/FeatureList.vue'
+// import GradedList from '@/components/front/Index/GradedList.vue'
+// import EvaluationBoard from '@/components/front/Index/EvaluationBoard.vue'
 </script>
--- src/views/front/TrailsIntroView.vue
+++ src/views/front/TrailsIntroView.vue
@@ -11,19 +11,23 @@
 <template>
   <div class="container d-grid gap-5">
     <h1 class="display-5 text-end text-primary py-4">主題分級</h1>
-    <RoleList />
-
-    <img src="/src/assets/illustration/acorn.svg" alt="icon" class="mx-auto" />
-    <div class="row">
+    <div class="py-15">
+      <RoleList />
+    </div>
+    <div class="row py-15">
       <div class="col-12 col-lg-8 mb-5 mb-lg-0 areaInfoBorder"><AreaList /></div>
       <div class="col-6 col-lg-2"><TypeList /></div>
       <div class="col-6 col-lg-2"><TourList /></div>
+    </div>
+    <div class="py-15">
+      <DifClassTable />
     </div>
   </div>
 </template>
 
 <script setup>
 import RoleList from '@/components/front/intro/RoleList.vue'
+import DifClassTable from '@/components/front/intro/DifClassTable.vue'
 import AreaList from '@/components/front/intro/AreaList.vue'
 import TypeList from '@/components/front/intro/TypeList.vue'
 import TourList from '@/components/front/intro/TourList.vue'
