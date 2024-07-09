dcecc9d5e584e1685e0502d4fa8f92c7afb681cb
kathyhu91 <huchiachun@gmail.com>
2024-07-09 12:27:51 +0800

    fix(TaskFormRating): Ratings stars should be selected or deselected in sequence

--- src/components/dashboard/task/form/TaskFormRating.vue
+++ src/components/dashboard/task/form/TaskFormRating.vue
@@ -18,13 +18,14 @@
   <div class="mb-5 d-flex align-items-center">
     <label class="form-label fs-5 me-3">{{ ratingConfig.title }}</label>
     <div>
-      <template v-for="item in ratingBaseNum" :key="item">
+      <template v-for="(item, index) in ratingBaseNum" :key="item">
         <input
           type="checkbox"
           class="btn-check"
           :id="`rating-${item}`"
           v-model="ratingModel[item - 1]"
           @change="updateRating(item)"
+          :disabled="isDisabled(index)"
         />
         <label
           :for="`rating-${item}`"
@@ -86,13 +87,20 @@ watch(
   { immediate: true }
 )
 
+function isDisabled(index) {
+  if (index === 0) {
+    return false
+  }
+  return !ratingModel.value[index - 1]
+}
+
 function updateRating(index) {
-  if (ratingModel[index - 1]) {
-    for (let i = 0; i < index; i++) {
+  if (ratingModel.value[index]) {
+    for (let i = 0; i <= index; i++) {
       ratingModel.value[i] = true
     }
   } else {
-    for (let i = index; i < ratingModel.value.length; i++) {
+    for (let i = index + 1; i < ratingModel.value.length; i++) {
       ratingModel.value[i] = false
     }
   }
