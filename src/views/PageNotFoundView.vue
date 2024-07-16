925c050bf89a446a29545472c1df587952bd1ebd
kathyhu91 <huchiachun@gmail.com>
2024-07-16 10:29:31 +0800

    fix: PageNotFoundView page and routing; add data loading in FrontLayoutView

---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,36 @@
+<style lang="scss" scoped>
+.err-img {
+  width: 50%;
+  max-width: 400px;
+}
+</style>
+
+<template>
+  <div class="container py-16">
+    <div class="vh-75 d-flex flex-column justify-content-center align-items-center">
+      <h1 class="fs-6 brand__img brand--sm mb-8">{{ pageNotFoundConfig.pageTitle }}</h1>
+      <p class="display-5 fw-bolder mb-8">{{ pageNotFoundConfig.info.mes }}</p>
+      <RouterLink class="btn btn-primary mb-8" :to="pageNotFoundConfig.info.guide.to">{{
+        pageNotFoundConfig.info.guide.title
+      }}</RouterLink>
+      <img :src="getImageUrl(pageNotFoundConfig.info.img)" alt="icon" class="err-img" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+import { getImageUrl } from '@/utils/imgUrl.js'
+
+const pageNotFoundConfig = {
+  pageTitle: '郊友趣・Hiking',
+  info: {
+    img: 'assets/illustration/road.png',
+    mes: '哎呀！好像找不到路呢',
+    guide: {
+      title: '回到登山口',
+      to: { name: 'FrontIndex' }
+    }
+  }
+}
+</script>
